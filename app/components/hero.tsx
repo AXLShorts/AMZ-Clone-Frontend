1072abadcb94c8d4d6defa8a45df28d1f486beb4
AXL <116296322+AXLShorts@users.noreply.github.com>
2025-12-06 16:00:04 +0500

    chore: update next and react versions in package.json

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -43,7 +43,7 @@ const Hero = () => {
           <h1 className="font-display text-3xl sm:text-4xl lg:text-5xl xl:text-6xl leading-tight tracking-[-1px] uppercase font-semibold z-20">
             You don&apos;t need more traffic{" "}
             <span className="text-brand-primary relative inline-block bg-[url('/assets/homepage/hero/orange-line.png')] bg-no-repeat bg-bottom bg-contain pb-2">
-              you need the right trafix and better conversion
+              you need the right traffic and better conversion
             </span>
           </h1>
 
